1874912f8e9258e1b49ecdbd7da0e2a07f94324d
Nikolrize <nicolechen119@gmail.com>
2026-04-18 19:30:57 +0800

    Add: login and signup UI

--- app/(pages)/signup/page.tsx
+++ app/(pages)/signup/page.tsx
@@ -10,14 +10,14 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
   return (
-    <main className="w-full h-screen flex items-center justify-center p-15">
+    <div className="w-full h-screen flex items-center justify-center p-15">
       <Card className="w-100 gap-6">
         <CardHeader>
-          <CardTitle>Login</CardTitle>
+          <CardTitle>Sign Up</CardTitle>
           <CardDescription>
-            Enter your username and password to login to your account
+            Fill in the fields to sign up for a new account
           </CardDescription>
         </CardHeader>
         <CardContent className="flex flex-col gap-6">
@@ -26,23 +26,29 @@ export default function Home() {
             <Input id="username" type="text" required />
           </div>
           <div className="flex flex-col gap-2">
+            <Label>Email</Label>
+            <Input id="email" type="email" required />
+          </div>
+          <div className="flex flex-col gap-2">
             <Label>Password</Label>
             <Input id="password" type="password" required />
           </div>
+          <div className="flex flex-col gap-2">
+            <Label>Confirm Password</Label>
+            <Input id="confirm-password" type="password" required />
+          </div>
 
           <div className="flex flex-col justify-center items-center gap-2">
-            <Button className="w-full" size={"lg"}>
-              Login
-            </Button>
+            <Button className="w-full" size={"lg"}>Create Account</Button>
             <div className="flex gap-2 text-muted-foreground">
-              <span>Don't have an account?</span>
-              <Link href={"/signup"} className="underline">
-                Sign up
+              <span>Already have an account?</span>
+              <Link href={"/"} className="underline">
+                Log in
               </Link>
             </div>
           </div>
         </CardContent>
       </Card>
-    </main>
+    </div>
   );
 }
